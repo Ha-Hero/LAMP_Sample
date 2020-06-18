512107b2a9fc28d205ac0bc07b87d4509302e025
Ha-Hero <h.hatakeyama.sys19@morijyobi.ac.jp>
2020-06-18 13:39:37 +0900

    add index.php

--- index.php
+++ index.php
@@ -19,17 +19,19 @@ try {
 
 <!DOCTYPE html>
 <html lang="ja">
+​
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>LAMP_Samp</title>
+    <title>LAMP SAMPLE PAGE</title>
 </head>
+​
 <body>
     <div class="container">
-        <? php foreach( $result as $value) {
+        <?php foreach ($result as $value) {
             echo "<h1>$value[id] $value[name] $value[age]</h1>";
-        }?>
+        } ?>
     </div>
 </body>
-</html>
+​
+</html
