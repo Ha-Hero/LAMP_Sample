9e80413220d5e743c07bcdf7b40471477875833b
Ha-Hero <h.hatakeyama.sys19@morijyobi.ac.jp>
2020-06-25 12:02:00 +0900

    index

--- index.php
+++ index.php
@@ -64,127 +64,253 @@ try {
     </div>
 
     <div class="container">
+
         <ul class="nav nav-tabs">
-         <li class="nav-items"><a href="#tab1" class ="nav-link" data-toggle="tab">select</a></li>
-         <li class="nav-items"><a href="#tab2" class ="nav-link" data-toggle="tab">insert</a></li>
-         <li class="nav-items"><a href="#tab3" class ="nav-link" data-toggle="tab">update</a></li>
-         <li class="nav-items"><a href="#tab4" class ="nav-link" data-toggle="tab">delete</a></li>
+
+            <li class="nav-item">
+
+                <a href="#tab1" class="nav-link active" data-toggle="tab">Select</a>
+
+            </li>
+
+            <li class="nav-item">
+
+                <a href="#tab2" class="nav-link" data-toggle="tab">Insert</a>
+
+            </li>
+
+            <li class="nav-item">
+
+                <a href="#tab3" class="nav-link" data-toggle="tab">Update</a>
+
+            </li>
+
+            <li class="nav-item">
+
+                <a href="#tab4" class="nav-link" data-toggle="tab">Delete</a>
+
+            </li>
+
         </ul>
 
+
+
         <div class="tab-content">
+
             <div id="tab1" class="tab-pane active">
-                <table class="table table-striped table-bordered mt-2">
+
+                <table class="table table-hover mt-2">
+
                     <caption>Show User Table</caption>
+
+
+
                     <thead class="thead-dark">
+
                         <tr>
+
                             <th>Id</th>
+
                             <th>Name</th>
+
                             <th>Age</th>
+
                         </tr>
+
                     </thead>
 
+
+
                     <tbody>
-                        <?php foreach($result as $value) {?>
-                        <tr>
-                            <th><?php echo "$value[id]" ?></th>
-                            <td><?php echo "$value[name]" ?></td>
-                            <td><?php echo "$value[age]" ?></td>
-                        </tr>
+
+                        <?php foreach ($result as $value) { ?>
+
+                            <tr>
+
+                                <th><?php echo "$value[id]" ?></th>
+
+                                <td><?php echo "$value[name]" ?></td>
+
+                                <td><?php echo "$value[age]" ?></td>
+
+                            </tr>
+
                         <?php } ?>
+
                     </tbody>
 
-                </table>    
+                </table>
+
             </div>
-        </div>
+
             <div id="tab2" class="tab-pane">
-                <form action="./insert.php" class="mt-5" method="POST">
+
+                <form class="mt-5" action="./insert.php" method="POST">
 
                     <div class="form-group row">
-                        <label for="id" class="col-sm-2 col-form-label">ID</label>
-                        <div class="col-sm-10" >
-                            <input class="form-control" type="text" name="id" id="id">
+
+                        <label class="col-sm-2 col-form-label" for="id">ID</label>
+
+                        <div class="col-sm-10">
+
+                            <input class="form-control" type="text" name="id" id="id" required>
+
                         </div>
+
                     </div>
 
                     <div class="form-group row">
-                        <label for="name" class="col-sm-2 col-form-label">NAME</label> 
-                        <div class="col-sm-10" >
-                            <input class="form-control" type="text" name="name" id="name">
+
+                        <label class="col-sm-2 col-form-label" for="name">Your Name</label>
+
+                        <div class="col-sm-10">
+
+                            <input class="form-control" type="text" name="name" id="name" required>
+
                         </div>
+
                     </div>
 
                     <div class="form-group row">
-                        <label for="age" class="col-sm-2 col-form-label">AGE</label> 
-                        <div class="col-sm-10" >
-                            <input class="form-control" type="text" name="age" id="age">
+
+                        <label class="col-sm-2 col-form-label" for="age">Age</label>
+
+                        <div class="col-sm-10">
+
+                            <input class="form-control" type="text" name="age" id="age" required>
+
                         </div>
+
                     </div>
 
-                    <button type="submit" class="btn btn-primary">insert</button>
-                </form>    
+
+
+                    <button type="submit" class="btn btn-block btn-primary">Insert</button>
+
+                </form>
+
             </div>
 
-            
             <div id="tab3" class="tab-pane">
-                <form action="./update.php" class="mt-5" method="POST">
+
+                <form class="mt-5" action="./update.php" method="POST">
 
                     <div class="form-group row">
-                        <label for="id" class="col-sm-2 col-form-label">ID</label>
-                        <div class="col-sm-10" >
-                            <input class="form-control" type="text" name="id" id="id">
+
+                        <label class="col-sm-2 col-form-label" for="id">ID</label>
+
+                        <div class="col-sm-10">
+
+                            <input class="form-control" type="text" name="id" id="id" required>
+
                         </div>
+
                     </div>
 
                     <div class="form-group row">
-                        <label for="name" class="col-sm-2 col-form-label">NAME</label> 
-                        <div class="col-sm-10" >
-                            <input class="form-control" type="text" name="name" id="name">
+
+                        <label class="col-sm-2 col-form-label" for="name">Your Name</label>
+
+                        <div class="col-sm-10">
+
+                            <input class="form-control" type="text" name="name" id="name" required>
+
                         </div>
+
                     </div>
 
                     <div class="form-group row">
-                        <label for="age" class="col-sm-2 col-form-label">AGE</label> 
-                        <div class="col-sm-10" >
-                            <input class="form-control" type="text" name="age" id="age">
+
+                        <label class="col-sm-2 col-form-label" for="age">Age</label>
+
+                        <div class="col-sm-10">
+
+                            <input class="form-control" type="text" name="age" id="age" required>
+
                         </div>
+
                     </div>
 
-                    <button type="submit" class="btn btn-primary">update</button>
-                </form>    
+
+
+                    <button type="submit" class="btn btn-block btn-primary">Update</button>
+
+                </form>
+
             </div>
 
             <div id="tab4" class="tab-pane">
+
                 <table class="table table-hover mt-2">
+
+
+
                     <thead class="thead-dark">
+
                         <tr>
+
                             <th>Id</th>
+
                             <th>Name</th>
+
                             <th>Age</th>
+
                             <th>-</th>
+
                         </tr>
+
                     </thead>
+
+
+
                     <tbody>
+
                         <?php foreach ($del_list as $value) { ?>
+
                             <tr>
+
                                 <th><?php echo "$value[id]" ?></th>
+
                                 <td><?php echo "$value[name]" ?></td>
+
                                 <td><?php echo "$value[age]" ?></td>
+
                                 <td>
+
                                     <form action="./delete_demo.php" method="$_GET">
+
                                         <input type="text" class="d-none" name="id" value="<?php echo "$value[id]" ?>">
+
                                         <button type="submit" class="btn btn-danger">delete</button>
+
                                     </form>
+
                                 </td>
+
                             </tr>
+
                         <?php } ?>
+
                     </tbody>
+
                 </table>
+
             </div>
-           
+
+        </div>
+
     </div>
 
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
 </body>
+
+
+
 </html>
